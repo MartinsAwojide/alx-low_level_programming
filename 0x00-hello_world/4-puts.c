bcfd3fd1640f6d86e16e120a1f2270905cb4b683
MartinsAwojide <awojidemartins@gmail.com>
2022-06-30 21:34:53 -0700

    quick puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main: calls puts function
+ * Description: this program prints the string in the puts function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programing is like building a multilingual puzzle");
+	return (0);
+}
