bf449942503a3903af4d6a4e15a69cfeaff11d59
MartinsAwojide <awojidemartins@gmail.com>
2022-07-06 09:26:12 -0700

    list numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: print list numbers
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
