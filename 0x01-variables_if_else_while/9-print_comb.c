724b828d2a5d16c9e0c350cb3108e60cc1a65c76
MartinsAwojide <awojidemartins@gmail.com>
2022-07-06 10:46:59 -0700

    list base_16

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,17 @@ int main(void)
 
 	for (i = '0' ; i <= '9' ; i++)
 	{
-	putchar(i);
-	putchar(',');
-	putchar(' ');
-	}
 
+		putchar(i);
+
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+		
 	putchar('\n');
 
 	return (0);
