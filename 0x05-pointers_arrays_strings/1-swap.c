5596cd5c5ee4e344f2efb25497204f2016e58ba3
MartinsAwojide <awojidemartins@gmail.com>
2022-07-13 11:07:34 -0700

    swap quick

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,16 +4,12 @@
  *  reset_to_98 - updates var value to reset_to_98
  *  @a: first variable
  *  @b: second variable
- *  @p: placeholder variable
- *  @q: placeholder variable
  *  Return: void on Success
  */
 
 void swap_int(int *a, int *b)
-{	
-	int p = *a;
-	int q = *b;
-
-	p = *b;
-	q = *a;	
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
 }
