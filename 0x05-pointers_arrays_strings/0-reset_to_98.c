40340219f631c590ca6cde7497eea4ae8571535c
MartinsAwojide <awojidemartins@gmail.com>
2022-07-13 10:17:58 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  reset_to_98 - updates var value to reset_to_98
+ *  @n: variable for execution
+ *  Return: 0 on Success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return (0);
+}
