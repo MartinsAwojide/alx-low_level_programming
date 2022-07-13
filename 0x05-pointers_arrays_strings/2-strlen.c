0889c9360c7d6589936f6b548c333109b94b85fc
MartinsAwojide <awojidemartins@gmail.com>
2022-07-13 11:31:04 -0700

    sting length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  _strlen - returns length of string
+ *  @s: pointer to string
+ *  Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(char *s);
+
+	return (len);
+}
