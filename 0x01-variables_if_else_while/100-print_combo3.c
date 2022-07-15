7246ce782a465497adb3fec831fb7ff582477854
MartinsAwojide <awojidemartins@gmail.com>
2022-07-15 16:34:39 -0700

    two digit combo

---
+++ 0x01-variables_if_else_while/100-print_combo3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - print possible combo of two digit numbers
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int i, n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (i > n)
+			{
+			putchar(n);
+			putchar(i);
+			}
+
+			if (n != '8' || (n == '8' && i != '9'))
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
